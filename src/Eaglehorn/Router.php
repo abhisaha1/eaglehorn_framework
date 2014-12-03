b16750043092caa386e0d6cb64f6e1f65dd2ef76
Abhishek Saha <abhisheksaha11@gmail.com>
2014-12-03 12:53:33 +0530

    Fixed router for fancy url

--- src/Eaglehorn/Router.php
+++ src/Eaglehorn/Router.php
@@ -96,8 +96,7 @@ class Router
                 if (preg_match($source, $request, $matches)) {
                     // A routing rule was matched
                     $matched_route = TRUE;
-                    unset($matches[0]);
-                    self::$_attr = array_filter(array_keys($matches), "is_int");
+                    self::$_attr = array_intersect_key($matches, array_flip(array_filter(array_keys($matches), 'is_string')));
                     self::_set_callback($destination);
                 }
             }
